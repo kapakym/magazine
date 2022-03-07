a134d77bdccf21ed22c4e60e31824274b71544f1
Alexey P <mycop14@mail.ru>
2022-03-07 16:54:42 +0300

    Описаны связи между моделями

--- server/models/models.js
+++ server/models/models.js
@@ -1,6 +1,7 @@
 const sequelize = require("../db");
 const { DataTypes } = require("sequelize");
 
+// Описание моделей
 const User = sequelize.define("user", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   email: { type: DataTypes.STRING, unique: true },
@@ -35,3 +36,51 @@ const Brand = sequelize.define("brand", {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   name: { type: DataTypes.STRING, unique: true, allowNull: false },
 });
+
+const Rating = sequelize.define("rating", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  rate: { type: DataTypes.INTEGER, allowNull: false },
+});
+
+const DeviceInfo = sequelize.define("deviceinfo", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  title: { type: DataTypes.STRING, allowNull: false },
+  description: { type: DataTypes.STRING, allowNull: false },
+});
+
+//# Промежуточная таблица для хранения связей
+const TypeBrand = sequelize.define("typebrand", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+// Описание связей между моделями
+User.hasOne(Basket); // Одна запись в БД содержит одну корзину
+Basket.belongsTo(User); // Корзина прирнадлежит пользователю
+
+User.hasMany(Rating);
+Rating.belongsTo(User);
+
+Basket.hasMany(BasketDevice);
+BasketDevice.belongsTo(Basket);
+
+Type.hasMany(Device);
+Device.belongsTo(Type);
+
+Brand.hasMany(Device);
+Device.belongsTo(Brand);
+
+Device.hasMany(Rating);
+Rating.belongsTo(Device);
+
+Device.hasMany(BasketDevice);
+BasketDevice.belongsTo(Device);
+
+Device.hasMany(DeviceInfo); // Одна запись в БД содержит много записей с характеристиками
+DeviceInfo.belongsTo(Device);
+
+Type.belongsToMany(Brand, { through: TypeBrand });
+Brand.belongsToMany(Type, { through: TypeBrand });
+
+module.exports = {
+    User, Basket, BasketDevice, Device, Type, Brand, Rating, TypeBrand, DeviceInfo
+}
