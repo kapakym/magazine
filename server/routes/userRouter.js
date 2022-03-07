fe350b3a82ef57e8ca1ad204b873f85ca6985674
Alexey P <mycop14@mail.ru>
2022-03-07 22:00:52 +0300

    Добавил контроллеры и error

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,10 +1,9 @@
 const { Router } = require("express");
 const router = new Router();
+const userController = require("../controllers/userController");
 
-router.post("/registration"); // Метод post для регистрации пользователя
-router.get("/login"); // Метод post для входа в личный кабинет
-router.get("/auth", (req, res) => {
-  res.json({ message: "auth" });
-}); // Метод get проверки авторизован пользователь или нет
+router.post("/registration", userController.registration); // Метод post для регистрации пользователя
+router.get("/login", userController.login); // Метод post для входа в личный кабинет
+router.get("/auth", userController.check); // Метод get проверки авторизован пользователь или нет
 
 module.exports = router;
