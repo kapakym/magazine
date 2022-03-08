0580d35f103e1233bec877b1882d3f20ed47fc7b
Alexey P <mycop14@mail.ru>
2022-03-08 21:47:21 +0300

    Добавил регистрацию пользователей

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,19 +1,41 @@
-const { query } = require('../db');
-const ApiError = require('../error/apiError')
+const ApiError = require("../error/apiError");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User, Basket } = require("../models/models");
+
 class UserController {
-  async registration(req, res) {}
+  async registration(req, res) {
+    const { email, password, role } = req.body;
+    if (!email || !password) {
+      return next(ApiError.badRequest("Некорректный email или пароль"));
+    }
+    const candidate = await User.findOne({ where: { email } });
+    if (candidate) {
+      return next(
+        ApiError.badRequest("Пользователь с таким email уже существует")
+      );
+    }
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ email, role, password: hashPassword });
+    const basket = await Basket.create({ userId: user.id });
+    const token = jwt.sign(
+      { id: user.id, email, role },
+      process.env.SECRET_KEY,
+      { expiresIn: "24h" }
+    );
+    return res.json({ token });
+  }
 
   async login(req, res) {}
 
   async check(req, res, next) {
     const { id } = req.query;
-    console.log(req.query)
+    console.log(req.query);
     if (!id) {
-      return next(ApiError.badRequest('Не задан ID'));
+      return next(ApiError.badRequest("Не задан ID"));
     }
     res.json(id);
   }
-
 }
 
 module.exports = new UserController();
