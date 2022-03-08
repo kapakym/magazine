1d1512dde5ab28ef200aad29ef5656e8315d922e
Alexey P <mycop14@mail.ru>
2022-03-08 10:42:19 +0300

    Исправил баг в обработчике ошибок

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,15 +1,19 @@
+const { query } = require('../db');
 const ApiError = require('../error/apiError')
 class UserController {
   async registration(req, res) {}
 
   async login(req, res) {}
+
   async check(req, res, next) {
     const { id } = req.query;
+    console.log(req.query)
     if (!id) {
       return next(ApiError.badRequest('Не задан ID'));
     }
-    res.json(query);
+    res.json(id);
   }
+
 }
 
 module.exports = new UserController();
