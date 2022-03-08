1a4409d4efd55f0122a92a08b6d5fe35aff1b1fb
Alexey P <mycop14@mail.ru>
2022-03-08 22:10:55 +0300

    Добавил функцию авторизации пользователя

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,12 +4,10 @@ const jwt = require("jsonwebtoken");
 const { User, Basket } = require("../models/models");
 
 const generateJwt = (id, email, role) => {
-  return jwt.sign(
-    { id, email, role },
-    process.env.SECRET_KEY,
-    { expiresIn: "24h" }
-  );
-}
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+};
 
 class UserController {
   async registration(req, res) {
@@ -26,11 +24,25 @@ class UserController {
     const hashPassword = await bcrypt.hash(password, 5);
     const user = await User.create({ email, role, password: hashPassword });
     const basket = await Basket.create({ userId: user.id });
-    const token = generateJwt(user.id, user.email, user.role)
+    const token = generateJwt(user.id, user.email, user.role);
     return res.json({ token });
   }
 
-  async login(req, res) {}
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: {email} });
+    if (!user) {
+      return next(
+        ApiError.internal("Пользователь с таким email не зарегистрирован")
+      );
+    }
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.internal("Указан неверный пароль"));
+    }
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({token});
+  }
 
   async check(req, res, next) {
     const { id } = req.query;
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,7 +3,7 @@ const router = new Router();
 const userController = require("../controllers/userController");
 
 router.post("/registration", userController.registration); // Метод post для регистрации пользователя
-router.get("/login", userController.login); // Метод post для входа в личный кабинет
+router.post("/login", userController.login); // Метод post для входа в личный кабинет
 router.get("/auth", userController.check); // Метод get проверки авторизован пользователь или нет
 
 module.exports = router;
