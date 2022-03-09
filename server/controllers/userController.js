cf871c912be0297d9efbb3a0a3002259dc827b9c
Alexey P <mycop14@mail.ru>
2022-03-09 16:56:31 +0300

    Добавлена возможность создавать категории и пр только авторизированным пользователям

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -45,12 +45,7 @@ class UserController {
   }
 
   async check(req, res, next) {
-    const { id } = req.query;
-    console.log(req.query);
-    if (!id) {
-      return next(ApiError.badRequest("Не задан ID"));
-    }
-    res.json(id);
+    res.json({ message: "Все работает" });
   }
 }
 
